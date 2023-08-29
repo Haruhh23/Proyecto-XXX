b4e4e9dbca33405a1790f27396263e6257b4fc8d
Haruhh23 <143463450+Haruhh23@users.noreply.github.com>
2023-08-29 01:56:55 +0000

    Se agrego libreria iostream

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char const *argv[])
 {
     /* code */
      std::cout<<"EL HECTOR SE ÑLA COME"<<std::endl;
+
+     std::cout<<"EL HECTOR SE LA COME"<<std::endl;
     return 0;
 }
 
